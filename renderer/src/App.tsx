456213f4737c10cf1ed45db0bc8ef45d3701f096
sam0ed <samoed.prime@gmail.com>
2024-01-20 16:27:20 +0100

    Initial commit, defualt window working

---
+++ renderer/src/App.tsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Crossword from '@jaredreisinger/react-crossword';
+
+const data = {
+  across: {
+    1: {
+      clue: 'one plus one',
+      answer: 'TWO',
+      row: 0,
+      col: 0,
+    },
+  },
+  down: {
+    2: {
+      clue: 'three minus two',
+      answer: 'ONE',
+      row: 0,
+      col: 2,
+    },
+  },
+}
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+      <Crossword data={data} />
+    </div>
+  );
+}
+
+export default App;

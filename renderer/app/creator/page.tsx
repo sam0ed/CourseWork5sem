0b5975cc649e3b190bf4badb95b801d400783fd9
mx_lshk <max.loshak.i@gmail.com>
2024-01-21 16:00:23 +0200

    minor changes �

---
+++ renderer/app/creator/page.tsx
@@ -0,0 +1,32 @@
+"use server"
+import React, { useState, useEffect } from 'react'
+import { createRoot } from 'react-dom/client'
+import Markdown from 'react-markdown'
+import fs from 'fs';
+
+// const markdown = '# Hi, *Pluto*!'
+
+// createRoot(document.body).render(<Markdown>{markdown}</Markdown>)
+
+export default function Creator() {
+    // const [fileContent, setFileContent] = useState('');
+
+    // useEffect(() => {
+    //     fetch('../../public/Creator.md')  // Adjust the path to your file
+    //         .then(response => response.text())
+    //         .then(text => {
+    //             setFileContent(text);
+    //         })
+    //         .catch(error => {
+    //             console.error('Error fetching the file:', error);
+    //         });
+    // }, []);
+
+
+    const fileContent = fs.readFileSync('../../public/Creator.md', 'utf8');
+
+    return (
+        <Markdown>{fileContent}</Markdown>
+    );
+}
+
--- renderer/src/screens/TypeDefault/index.ts
+++
@@ -1,1 +0,0 @@
-export { TypeDefault } from "./TypeDefault";
